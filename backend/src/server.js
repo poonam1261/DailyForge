f040dced8db9aab37685d5fd37af4e604b9aa129
unknown <pprabhugaonkar6@nitgoa.ac.in>
2026-05-17 11:45:47 +0530

    changed variables and url to match my local settings

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const app = express();
 // Intialize cors
 app.use(
   cors({
-     origin: "https://dailyforge-frontend-lhjq.onrender.com",
+     origin: "http://localhost:5173",
     credentials: true,
   })
 );
